df1f3e6f4ca32e6f9d304aa4d2526b448b230569
gaurav20619 <gauravsinghraijada619@gmail.com>
2023-03-19 12:19:40 +0530

    Add: first commit

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabinvoicegeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinvoicegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinvoicegeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/CabinvoicegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinvoicegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
